ad10ee6c30d1bd553d24d1703e58e6c691cef929
Tim Maloshtan <tim.maloshtan@gmail.com>
2020-09-07 07:58:54 +0300

    Refetch playlist items

--- src/api/routes/playlists.ts
+++ src/api/routes/playlists.ts
@@ -1,9 +1,32 @@
 import { Router, Request, Response } from "express";
 import url from "url";
-import { google } from "googleapis";
+import { google, youtube_v3 } from "googleapis";
 import config from "../../config";
 
 const route = Router();
+
+async function* fetchYouTubePlaylistItems(
+  options: youtube_v3.Params$Resource$Playlistitems$List
+): AsyncGenerator<youtube_v3.Schema$PlaylistItem, void, unknown> {
+  const youtube = google.youtube("v3");
+
+  const response = await youtube.playlistItems.list(options);
+
+  const nextPageToken = response?.data?.nextPageToken;
+
+  const items = response?.data?.items || [];
+
+  for (const item of items) {
+    yield item;
+  }
+
+  if (nextPageToken) {
+    yield* fetchYouTubePlaylistItems({
+      ...options,
+      pageToken: nextPageToken,
+    });
+  }
+}
 
 export default (app: Router) => {
   app.use("/playlists", route);
@@ -12,15 +35,23 @@ export default (app: Router) => {
     const urlObject = new url.URL(req.body.url as string);
     const playlistId = urlObject.searchParams.get("list") as string;
 
-    const youtube = google.youtube("v3");
+    const items = [];
 
-    const result = await youtube.playlistItems.list({
+    const generator = fetchYouTubePlaylistItems({
       playlistId,
       part: ["snippet", "contentDetails"],
       key: config.youtube.key,
       maxResults: 50,
     });
 
-    res.status(200).json(result);
+    for await (const item of generator) {
+      items.push({
+        title: item.snippet?.title,
+        position: item.snippet?.position,
+        videoId: item.contentDetails?.videoId,
+      });
+    }
+
+    res.status(200).json(items);
   });
 };
